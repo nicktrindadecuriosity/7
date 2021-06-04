ffaeb904e0fbaa137ea96ec4bcf8b3d0321ef2c4
nicktrindadecuriosity <85320885+nicktrindadecuriosity@users.noreply.github.com>
2021-06-03 21:36:28 -0300

    Add files via upload

---
+++ Script4.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function check(){
+
+var question5 = document.question5.deusa.value; 
+
+	if (question5 == "Vesta") { document.getElementById("certo1").style.visibility = "visible"; document.getElementById("falha").style.visibility = "hidden"; }
+		else { document.getElementById("falha").style.visibility = "visible"; document.getElementById("certo1").style.visibility = "hidden";
+}
+	}
